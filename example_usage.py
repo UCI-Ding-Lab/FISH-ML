89c3f8446b8218345cae899ab198a9a26ffb18cd
Yicheng Ding <yiched4@uci.edu>
2024-06-26 19:46:16 +0800

    Backend Creation
    - merge all sections in .predict()
    - error check and logging

---
+++ example_usage.py
@@ -0,0 +1,11 @@
+import fishCore
+import pathlib
+import cv2
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+fishcore = fishCore.fishcore(pathlib.Path("./config.ini"))
+fishcore.set_modle_version("2.1")
+img = cv2.cvtColor(np.array(Image.open(pathlib.Path("./img/test_1.jpg"))), cv2.COLOR_GRAY2RGB).astype(np.uint8)
+fishcore.predict(img)
---
+++ fishCore.py
@@ -0,0 +1,151 @@
+import pathlib
+import logging
+import configparser
+from sklearn.cluster import DBSCAN
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from transformers import SamModel, SamConfig, SamProcessor
+from scipy.ndimage import zoom
+
+
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        'DEBUG': '\033[94m',
+        'INFO': '\033[92m',
+        'WARNING': '\033[93m',
+        'ERROR': '\033[91m',
+        'CRITICAL': '\033[1;91m',
+        'RESET': '\033[0m'
+    }
+
+    def format(self, record):
+        log_color = self.COLORS.get(record.levelname, self.COLORS['RESET'])
+        reset_color = self.COLORS['RESET']
+        record.levelname = f"{log_color}{record.levelname}{reset_color}"
+        record.msg = f"{log_color}{record.msg}{reset_color}"
+        return super().format(record)
+
+class fishcore():
+    def __init__(self,config: pathlib.Path) -> None:
+        # config.ini
+        self.config = configparser.ConfigParser()
+        self.config.read(config)
+        # logger
+        self.logger = logging.getLogger('fishcore')
+        self.logger.setLevel(logging.INFO)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        formatter = ColoredFormatter("[%(asctime)s][%(levelname)s] %(message)s")
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+        # asset
+        self.asset_folder_path = pathlib.Path(self.config["general"]["asset_folder_path"])
+        self.supported_version = self.config["general"]["supported_version"].split(",")
+        self.model_version = None
+        self.model_path = None
+        # ai
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = None
+        self.model_config = SamConfig.from_pretrained("facebook/sam-vit-base")
+        self.processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
+        
+        #self.assets_validation()
+    
+    def set_modle_version(self,v):
+        if v in self.supported_version:
+            self.model_version = v
+            self.model_path = self.asset_folder_path / "modle" / f"fish_v{v}.pth"
+            if self.model:
+                del self.model
+            self.model = SamModel(config=self.model_config)
+            self.model.load_state_dict(torch.load(self.model_path, map_location=torch.device(self.device)))
+            self.model.to(self.device)
+            self.logger.info(f"set_modle_version: Modle version {v} loaded")
+        else:
+            self.logger.error(f"set_modle_version: Version {v} is not supported")
+    
+    def assets_validation(self):
+        modle_folder = self.asset_folder_path / "modle"
+        for v in self.supported_version:
+            if not (modle_folder / f"fish_{v}.pth").exists():
+                self.logger.error(f"assets_validation: Modle file for version {v} is missing")
+    
+    def predict(self, img: np.ndarray, show=False):
+        if not self.model:
+            self.logger.error("predict: Modle not loaded")
+            return
+        
+        def prepare_input(img):
+            def get_top_brightness_points(np_image, percentage):
+                grayscale_image = np.mean(np_image, axis=2)
+                threshold_value = np.percentile(grayscale_image, 100 - percentage)
+                bright_areas_mask = grayscale_image > threshold_value
+                return bright_areas_mask
+            
+            def cluster_points(points, eps=3, min_samples=5):
+                db = DBSCAN(eps=eps, min_samples=min_samples).fit(points)
+                labels = db.labels_
+                unique_labels = set(labels)
+                clusters = [points[labels == k] for k in unique_labels if k != -1]
+                return clusters
+            
+            def get_bounding_box(points):
+                min_x = np.min(points[:, 1])
+                max_x = np.max(points[:, 1])
+                min_y = np.min(points[:, 0])
+                max_y = np.max(points[:, 0])
+                return [min_x, min_y, max_x, max_y]
+            
+            bright_areas_mask = get_top_brightness_points(img, int(self.config["predict"]["brightest_percentage"]))
+            bright_points = np.column_stack(np.where(bright_areas_mask))
+            clusters = cluster_points(bright_points, eps=int(self.config["predict"]["dbscan_eps"]), min_samples=int(self.config["predict"]["dbscan_min_samples"]))
+            grouped_input_points = [cluster.tolist() for cluster in clusters]
+            bounding_boxes = [get_bounding_box(np.array(cluster)) for cluster in grouped_input_points]
+            return bounding_boxes
+        
+        bboxes = prepare_input(img)
+        self.logger.info(f"predict: Found {len(bboxes)} bounding boxes")
+        inputs = self.processor(img, input_boxes=[[bbox for bbox in bboxes]], return_tensors="pt")
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        self.model.eval()
+        with torch.no_grad():
+            outputs = self.model(**inputs, multimask_output=False)
+            # !!!pred_masks shape: torch.Size([1, 43, 1, 256, 256]) be4 squeeze
+        
+        result = np.zeros(img.shape[:2], dtype=np.uint8)
+        seg_result = []
+        zoom_factor = img.shape[0] / 256
+
+        for ind, bbox in enumerate(bboxes):
+            self.logger.info(f"predict: Processing bounding box {ind + 1}")
+            min_x, min_y, max_x, max_y = bbox
+            
+            # ignore noise
+            if max_x <= min_x or max_y <= min_y:
+                continue
+
+            # squeeze first [1 43 1 256 256] + loop, target -> [256 256] of prob
+            prob = torch.sigmoid(outputs.pred_masks.squeeze(1)).cpu().numpy().squeeze()[ind]
+            # filter threshold now [256 256] of 0 and 1
+            seg = (prob > float(self.config["predict"]["probability_threshold"])).astype(np.uint8)
+            # mag to 2048 2048
+            seg = zoom(seg, (zoom_factor, zoom_factor), order=0)
+            seg = seg[min_y:max_y, min_x:max_x]
+            result[min_y:max_y, min_x:max_x] = seg
+
+        if self.config["show"]["enable"].lower() == "true":
+            fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+            if self.config["show"]["enhancement"].lower() == "true":
+                axes[0].imshow(np.clip(np.array(img) * float(self.config["show"]["exposure_factor"]), 0, 65535), cmap='gray')
+            else:
+                axes[0].imshow(img)
+            axes[0].set_title("Image")
+            axes[1].imshow(result, cmap='gray')
+            axes[1].set_title("Mask")
+            plt.show()
+        
+        return seg
+        
+        
